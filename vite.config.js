ec2a27bf6c0e12e624317844d5213a9c7f92ae9e
Pan <uiok588@gmail.com>
2024-01-31 11:59:56 +0800

    給gitHub page 的專屬設定檔

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,26 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
+
+//電腦內開啟所使用
+//要使用這一區塊才可以跑所有vue3終端機指令
+/*
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+});
+*/
+
+//為了使用 gitHub pages 所設定
+
+export default defineConfig({
+  base: "ClassSchedule_DEMO",
+  plugins: [],
+  resolve: {
+    alias: [],
   },
 });
